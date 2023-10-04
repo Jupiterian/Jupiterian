77642bb89f207ae4dc552743d454b856ac89f449
Jupiterian <147007834+Jupiterian@users.noreply.github.com>
2023-10-04 16:52:13 -0700

    Add files via upload
    This is a .ps1 script that allows you to check the has of a file. You may get an error for certain algorithms if you have enabled FIPS cryptography.

---
+++ Hash Checker.ps1
@@ -0,0 +1,9 @@
+﻿$originalHash = Read-Host -Prompt "Enter original hash: "
+$FilePath = Read-Host -Prompt "Enter file path: "
+$Algorithm = Read-Host -Prompt "Enter algorithm: "
+$Hash = (Get-FileHash $FilePath -Algorithm $Algorithm | Select-Object "Hash").Hash
+if ($originalHash -eq $Hash) {
+    Write-Host "Checksum matched! File is correctly downloaded."
+} else {
+    Write-Host "Checksum not matched! File may be corrupt."
+}
