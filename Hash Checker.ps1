dab4d48490b087187adca18872a2a5f29613762d
Jupiterian <147007834+Jupiterian@users.noreply.github.com>
2023-10-04 17:04:41 -0700

    Update Hash Checker.ps1

--- Hash Checker.ps1
+++ Hash Checker.ps1
@@ -1,9 +1,13 @@
-﻿$originalHash = Read-Host -Prompt "Enter original hash: "
+#Gets Information from the User
+$originalHash = Read-Host -Prompt "Enter original hash: "
 $FilePath = Read-Host -Prompt "Enter file path: "
 $Algorithm = Read-Host -Prompt "Enter algorithm: "
+#Calculates Hash
 $Hash = (Get-FileHash $FilePath -Algorithm $Algorithm | Select-Object "Hash").Hash
+#Prints Results
 if ($originalHash -eq $Hash) {
     Write-Host "Checksum matched! File is correctly downloaded."
 } else {
     Write-Host "Checksum not matched! File may be corrupt."
+    Write-Host "The hash for $FilePath in $Algorithm is $Hash"
 }
